f623845a4ed47f3877a768d0945a5d36b246f12c
Beauty  priya <sbpriyaaparajita@gmail.com>
2023-06-16 18:37:05 +0530

    migration with user table

--- db/migrations.sql
+++ db/migrations.sql
@@ -1,5 +1,5 @@
 /* New column in calendar_event table*/
 ALTER TABLE `calendar_event` ADD `interviewer_id` INT(11) NULL DEFAULT NULL AFTER `public`;
 
-/* New column in cndidate table*/
-ALTER TABLE `user` ADD `is_interviewer` INT(11) NULL DEFAULT NULL AFTER `best_time_to_call`;
+/* New column in user table*/
+ALTER TABLE `user` ADD `is_interviewer` INT(11) NULL DEFAULT NULL AFTER `can_see_eeo_info`;
